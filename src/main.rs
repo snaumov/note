66b099727aea3e868381f81a2d2fb19efcbbf11d
Stepan Naumov <s.s.naumov@gmail.com>
2022-04-07 22:04:51 -0400

    Adding note using external editor #5

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,18 @@ use clap::{App, Arg};
 use config::{Config, FileSourceFile, File};
 use std::{fs::{OpenOptions}};
 use std::io::prelude::*;
+use std::env;
 use dirs::home_dir;
+use edit;
+
+fn set_editor(editor: Option<String>) -> impl Fn() -> () {
+    let default_editor = env::var("EDITOR");
+    if editor.is_some() { env::set_var("EDITOR", editor.unwrap()) };
+    move || match &default_editor {
+        Ok(editor) => { env::set_var("EDITOR", editor)},
+        Err(_) => env::remove_var("EDITOR"),
+    } 
+}
 
 fn main() {
     let mut settings = Config::new();
@@ -11,9 +22,10 @@ fn main() {
     let add_settings_res = settings.merge::<File<FileSourceFile>>(settings_file.into());
     if add_settings_res.is_err() { println!("Cannot find the config, the program might not work as expected")};
 
+
     let matches = App::new("Notes")
         .author("Stepan Naumov")
-        .version("0.0.2")
+        .version("0.0.3")
         .about("Note taking cmd line utility")
         .arg(Arg::new("note").index(1).multiple_occurrences(true))
         .arg(Arg::new("topic").short('t').long("topic").takes_value(true))
@@ -36,7 +48,26 @@ fn main() {
             // If no subcommand is provided, it's a saving note situtation
             let topic = matches.value_of("topic").expect("You must provide topic");
             let path = settings.get_str(&format!("topic.{}", topic)).expect("No topic is found in settings");
-            let text_to_write = matches.values_of("note").expect("no text to write").collect::<Vec<&str>>().join(" ");
+            let revert_editor = set_editor(settings.get_str("general.editor").ok());
+            let text_to_write = match matches.values_of("note") {
+                Some(text) => text.collect::<Vec<&str>>().join(" "),
+                None => {
+                    match edit::edit("") {
+                        Ok(text) => {
+                            if text == "" {
+                                println!("Nothing to save");
+                                return;
+                            }
+                            text
+                        },
+                        Err(_) => {
+                            println!("Nothing to save");
+                            return;
+                        },
+                    }
+                }
+            };
+
             let mut file = OpenOptions::new()
                 .append(true)
                 .create(true)
@@ -45,6 +76,7 @@ fn main() {
                 .expect("Cannot open the file for writing");
 
             writeln!(file, "{}", text_to_write).expect("Cannot write to the file");
+            revert_editor();
             println!("Succesfuly saved the note!");
         },
         _ => unreachable!("this should not happen")
